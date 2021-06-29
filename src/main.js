029a976ddd6275963802dbd23c7210ab0f98b11e
Mush <mush.hakobian@mail.ru>
2021-06-29 15:56:36 +0400

    uninstall prime

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import 'primeicons/primeicons.css'
 import './assets/style/m-style.css'
 import './assets/style/m-xs-style.css'
 import './assets/style/my-style.scss'
